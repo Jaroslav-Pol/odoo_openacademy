7a95064bfad26d727144d69ee6b0d16271a05681
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-04-26 16:22:08 +0300

    Added calendar view, also added different colors for different session duration

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 
@@ -44,6 +44,8 @@ class Session(models.Model):
     _description = "OpenAcademy Sessions"
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.Date.today)
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
@@ -54,6 +56,7 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -79,6 +82,27 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=record.duration, seconds=-1)
+            record.end_date = record.start_date + duration
+
+    def _set_end_date(self):
+        for record in self:
+            if not (record.start_date and record.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            record.duration = (record.end_date - record.start_date).days + 1
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
