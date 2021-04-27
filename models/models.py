8a904aebeb739bf9c034ee77a8272c943c075c0f
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-04-27 09:51:19 +0300

    Added graph view

--- models/models.py
+++ models/models.py
@@ -49,6 +49,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
